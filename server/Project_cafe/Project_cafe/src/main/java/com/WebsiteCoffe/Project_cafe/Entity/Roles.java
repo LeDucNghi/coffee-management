e94e1d05140aa986e084acc5894e8b5fcf87679d
phucpq07122000 <122593397+phucpq07122000@users.noreply.github.com>
2023-07-31 16:53:05 +0700

    update

--- server/Project_cafe/Project_cafe/src/main/java/com/WebsiteCoffe/Project_cafe/Entity/Roles.java
+++ server/Project_cafe/Project_cafe/src/main/java/com/WebsiteCoffe/Project_cafe/Entity/Roles.java
@@ -1,8 +1,14 @@
 package com.WebsiteCoffe.Project_cafe.Entity;
 
 
+import com.fasterxml.jackson.annotation.JsonIgnore;
 import jakarta.persistence.*;
 import lombok.*;
+import org.hibernate.annotations.Fetch;
+import org.hibernate.annotations.FetchMode;
+
+import java.util.HashSet;
+import java.util.Set;
 
 @Entity
 @Getter
@@ -17,8 +23,18 @@ public class Roles {
             name = "roles_sequence",
             sequenceName = "roles_sequence",
             allocationSize = 1)
-    @GeneratedValue(strategy = GenerationType.AUTO,
+    @GeneratedValue(
+            strategy = GenerationType.AUTO,
                     generator = "roles_sequence")
-    private long id;
+    private Long id;
     private String name;
+    @ManyToMany(mappedBy = "roles")
+    @Fetch(value= FetchMode.SELECT)
+    @JsonIgnore
+    private Set<Employees> employees = new HashSet<>();
+
+    public Roles(Long id, String name) {
+        this.id = id;
+        this.name = name;
+    }
 }
---
+++ server/Project_cafe/Project_cafe/src/main/java/com/WebsiteCoffe/Project_cafe/Service/EmployeeServiceImpl.java
@@ -0,0 +1,45 @@
+package com.WebsiteCoffe.Project_cafe.Service;
+
+
+import com.WebsiteCoffe.Project_cafe.Entity.Employees;
+import com.WebsiteCoffe.Project_cafe.Entity.Roles;
+import com.WebsiteCoffe.Project_cafe.Repository.EmployeeRepository;
+import com.WebsiteCoffe.Project_cafe.Repository.RoleRepository;
+import jakarta.transaction.Transactional;
+import lombok.RequiredArgsConstructor;
+import lombok.extern.slf4j.Slf4j;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.security.crypto.password.PasswordEncoder;
+import org.springframework.stereotype.Service;
+
+@Service
+@RequiredArgsConstructor
+@Transactional
+@Slf4j
+public class EmployeeServiceImpl implements EmployeeService{
+
+    @Autowired
+    private  EmployeeRepository employeeRepository;
+    @Autowired
+    private RoleRepository roleRepository;
+    private final PasswordEncoder passwordEncoder;
+
+    @Override
+    public Employees saveEmployee(Employees employees) {
+        employees.setEmployee_password(passwordEncoder.encode(employees.getEmployee_password()));
+        return employeeRepository.save(employees);
+    }
+
+    @Override
+    public Roles saveRoles(Roles roles) {
+       return  roleRepository.save(roles);
+    }
+
+    @Override
+    public void addToEmployee(String employeeName, String roleName) {
+
+        Employees employee= employeeRepository.findByEmployeeName(employeeName);
+        Roles role = roleRepository.findByName(roleName);
+        employee.getRoles().add(role);
+    }
+}
